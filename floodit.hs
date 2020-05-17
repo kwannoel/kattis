e0785ad26aea6af24c16f48ce52d59d747b23be4
Kwan Noel <noelkwan1998@gmail.com>
2020-05-17 18:53:51 +0800

    Parsing stable

--- floodit.hs
+++ floodit.hs
@@ -3,6 +3,7 @@
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE TypeApplications #-}
 
 {- |
@@ -78,60 +79,31 @@ import GHC.TypeNats
 import Text.ParserCombinators.ReadP
 
 data TestCase = TestCase 
-    { getSize :: Int
-    , getGrid :: Grid
+    { getGrid :: Grid
     } deriving (Show)
 
 type Solution = TestCase
 
 type Color = Char
-type Grid = [[Color]]
+type Grid = SomeGrid Color
 
 data SomeGrid a where
     SomeGrid :: KnownNat n => Vector n (Vector n a) -> SomeGrid a
 
+deriving instance Show a => Show (SomeGrid a)
+
 data SomeVector a where
     SomeVector :: KnownNat n => Vector n a -> SomeVector a
 
+deriving instance Show a => Show (SomeVector a)
+
 newtype Vector (n :: Nat) (a :: Type) = Vector 
     { unVector :: [a]
-    }
-
-mkSomeGrid :: KnownNat n => [Vector n a] -> Maybe (SomeGrid a)
-mkSomeGrid xs = case mkSomeVector xs of
-    SomeVector (v :: Vector m (Vector n a)) -> case sameNat (Proxy @m) (Proxy @n) of
-        Nothing -> Nothing
-        Just Refl -> Just $ SomeGrid v
+    } deriving (Show)
 
 mkSomeVector :: forall a . [a] -> SomeVector a
-mkSomeVector xs = case someNatVal (fromIntegral $ length xs) of 
+mkSomeVector xs = case someNatVal $ fromIntegral (length xs) of 
     SomeNat (_ :: Proxy n) -> SomeVector (Vector xs :: Vector n a)
-
-someGridP :: Int -> ReadP (SomeGrid Color)
-someGridP n = case someNatVal (fromIntegral n) of
-    SomeNat (_ :: Proxy n) -> 
-        let rowP :: ReadP (Vector n Color)
-            rowP = do
-                xs <- count n $ satisfy (`elem` "123456")
-                case mkSomeVector xs of
-                    SomeVector (v :: Vector m a) -> case sameNat (Proxy @m) (Proxy @n) of
-                        Nothing -> empty
-                        Just Refl -> pure v
-
-            gridP :: ReadP (SomeGrid Color)
-            gridP = do
-                xs <- countSepBy n newline rowP
-                case mkSomeGrid xs of
-                    Nothing -> empty
-                    Just g -> pure g
-
-        in gridP
-    where
-        countSepBy :: Int -> ReadP sep -> ReadP a -> ReadP [a]
-        countSepBy n sep p = (:) <$> p <*> replicateM (n - 1) (sep *> p)
-    
-        newline :: ReadP Char
-        newline = satisfy (== '\n')
 
 -- | Parse the input to a list of test cases
 parse :: String -> [TestCase]
@@ -146,16 +118,28 @@ parse s = fst $ head $ readP_to_S inputP s
             -- Parse test cases
             countSepBy numTestCases newline $ do
                 size <- sizeP <* newline
-                grid <- gridP size
-                pure $ TestCase size grid
+                grid <- someGridP size
+                pure $ TestCase grid
 
         numTestCasesP = int
         sizeP = int
 
-        gridP :: Int -> ReadP Grid
-        gridP n = countSepBy n newline rowP
-            where
-                rowP = count n $ satisfy (`elem` "123456")
+        someGridP :: Int -> ReadP (SomeGrid Color)
+        someGridP n = case someNatVal (fromIntegral n) of
+            SomeNat (_ :: Proxy n) -> do
+                let rowP :: ReadP (Vector n Color)
+                    rowP = do
+                        xs <- count n $ satisfy (`elem` "123456")
+                        case mkSomeVector xs of
+                            SomeVector (v :: Vector m Color) -> case sameNat (Proxy @m) (Proxy @n) of
+                                Nothing -> empty
+                                Just Refl -> pure v
+
+                rows <- countSepBy n newline rowP
+                case mkSomeVector rows of
+                    SomeVector (v :: Vector m (Vector n Color)) -> case sameNat (Proxy @m) (Proxy @n) of
+                        Nothing -> empty
+                        Just Refl -> pure $ SomeGrid v
 
         -- | @countSepBy n sep p@ parses @n@ occurrences of @p@ in sequence, separated by @sep@. A list of results is returned. 
         countSepBy :: Int -> ReadP sep -> ReadP a -> ReadP [a]
